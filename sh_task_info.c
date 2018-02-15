2e57ffdec369847ff43073664c98ece10cd24ad3
Deepak16031 <deepak16031@iiitd.ac.in>
2018-02-15 22:50:51 +0530

    Null file errno set

--- sh_task_info.c
+++ sh_task_info.c
@@ -31,7 +31,6 @@ asmlinkage long sys_sh_task_info(int pid, char *name)
 	char message[1000];
 	message[0] = ' ';
 	message[1] = '\0';
-//	char *data = task->comm;
 	mm_segment_t old_fs;// = get_fs();
 	struct pid *getpid;
 	struct task_struct *task;
@@ -49,13 +48,13 @@ asmlinkage long sys_sh_task_info(int pid, char *name)
 	strcat(message,"state is ");
 	sprintf(msgstate,"%ld",task->state);
 	sprintf(msgon_cpu,"%d",task->on_cpu);
-	sprintf(msgexec_start,"%lu",se->exec_start);
-	sprintf(msgsum_exec_runtime,"%lu",se->sum_exec_runtime);
-	sprintf(msgvruntime,"%lu",se->vruntime);
-	sprintf(msgprev_sum_exec_runtime,"%lu",se->prev_sum_exec_runtime);
-	sprintf(msgnr_migrations,"%lu",se->nr_migrations);
-	sprintf(msgsp0,"%lu",thread->sp0);
-	sprintf(msgsp1,"%lu",thread->sp);
+	sprintf(msgexec_start,"%llu",se->exec_start);
+	sprintf(msgsum_exec_runtime,"%llu",se->sum_exec_runtime);
+	sprintf(msgvruntime,"%llu",se->vruntime);
+	sprintf(msgprev_sum_exec_runtime,"%llu",se->prev_sum_exec_runtime);
+	sprintf(msgnr_migrations,"%llu",se->nr_migrations);
+	sprintf(msgsp0,"%llu",thread->sp0);
+	sprintf(msgsp1,"%llu",thread->sp);
 	strcat(message,msgstate);
 	strcat(message,"state :");
 	strcat(message,msgstate);
@@ -93,13 +92,13 @@ asmlinkage long sys_sh_task_info(int pid, char *name)
 	printk(KERN_ALERT"value of rt_priority is %d\n",task->rt_priority);
 	printk(KERN_ALERT"value of tgid is %d\n",task->tgid);
 	printk(KERN_ALERT"value of on_cpu is %d\n",task->on_cpu);
-	printk(KERN_ALERT"value of exec_start is %lu\n",se->exec_start);
-	printk(KERN_ALERT"value of sum_exec_runtime is %lu\n",se->sum_exec_runtime);
-	printk(KERN_ALERT"value of vruntime is %lu\n",se->vruntime);
-	printk(KERN_ALERT"value of prev_sum_exec_runtime is %lu\n",se->prev_sum_exec_runtime);
-	printk(KERN_ALERT"nr_migrations = %lu\n",se->nr_migrations);
-	printk(KERN_ALERT"sp0 is %lu\n",thread->sp0);
-	printk(KERN_ALERT"sp is %lu\n",thread->sp);
+	printk(KERN_ALERT"value of exec_start is %llu\n",se->exec_start);
+	printk(KERN_ALERT"value of sum_exec_runtime is %llu\n",se->sum_exec_runtime);
+	printk(KERN_ALERT"value of vruntime is %llu\n",se->vruntime);
+	printk(KERN_ALERT"value of prev_sum_exec_runtime is %llu\n",se->prev_sum_exec_runtime);
+	printk(KERN_ALERT"nr_migrations = %llu\n",se->nr_migrations);
+	printk(KERN_ALERT"sp0 is %llu\n",thread->sp0);
+	printk(KERN_ALERT"sp is %llu\n",thread->sp);
 //	printk("sysenter_cs is %lu\n",thread->sysenter_cs);
 //	printk("value of blocked is %s\n",strsignal(task->blocked));
 //	printk("value of real_blocked is %x\n",task->real_blocked);
@@ -107,7 +106,7 @@ asmlinkage long sys_sh_task_info(int pid, char *name)
 	old_fs = get_fs();
 	set_fs(KERNEL_DS);
 	// *data = "write to file";
-	fd = sys_open(name,O_WRONLY|O_CREAT,0644);
+	fd = sys_open(name,O_WRONLY,0644);
 	if (fd >=0)
 	{
 		sys_write(fd,message,strlen(message));
